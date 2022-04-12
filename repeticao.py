00f279621f960c27002c02310cb0336d127c8a42
LucasGuerraS <rm95084@fiap.com.br>
2022-04-12 11:23:09 -0300

    tratamento de loops infinitos

--- repeticao.py
+++ repeticao.py
@@ -1,5 +1,6 @@
 x = int(input("Qunatas vezes você deseja imprimir a frase: "))
-
+while x < 0:
+    x = int(input("Qunatas vezes você deseja imprimir a frase: "))
 while x != 0:
     x -= 1
-    print("Pedro!!")
+    print("Oi!!")
---
+++ repeticao3.py
@@ -0,0 +1,6 @@
+inicio = int(input("Incio: "))
+fim = int(input("Fim: "))
+x = inicio
+while x <= fim:
+    print(str(x))
+    x += 1
