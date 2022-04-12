1f4ca5b17964a47dfde20b06b71d992479da1781
LucasGuerraS <rm95084@fiap.com.br>
2022-04-12 11:25:29 -0300

    ajeitei a semântica

--- repeticao.py
+++ repeticao.py
@@ -1,6 +1,6 @@
-x = int(input("Qunatas vezes você deseja imprimir a frase: "))
+x = int(input("Quantas vezes você deseja imprimir a frase: "))
 while x < 0:
-    x = int(input("Qunatas vezes você deseja imprimir a frase: "))
+    x = int(input("Por favor coloque um número maior que 0: "))
 while x != 0:
     x -= 1
     print("Oi!!")
