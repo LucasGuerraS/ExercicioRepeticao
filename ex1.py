f4c2479620db934b81bb171dd5c93f7668fa9bf4
LucasGuerraS <rm95084@fiap.com.br>
2022-04-12 11:39:14 -0300

    exercicios em sala

---
+++ ex1.py
@@ -0,0 +1,4 @@
+x = 200
+while x >= 100:
+    print(str(x))
+    x -= 1
---
+++ ex2.py
@@ -0,0 +1,11 @@
+nums = int(input("Digite quantos números pares dseja que sejam impressos: "))
+x = 0
+while nums != 0:
+    print(x)
+    x += 2
+    if nums > 0:
+       nums -= 1
+
+    elif nums < 0:
+       nums += 1
+
