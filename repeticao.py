fcb9be00382a82a4179d4055e23acc703a68b69b
LucasGuerraS <rm95084@fiap.com.br>
2022-04-12 11:13:44 -0300

    first commit

---
+++ repeticao.py
@@ -0,0 +1,5 @@
+x = int(input("Qunatas vezes você deseja imprimir a frase: "))
+
+while x != 0:
+    x -= 1
+    print("Pedro!!")
---
+++ repeticao2.py
@@ -0,0 +1,8 @@
+x = int(input("Valor: "))
+cont = 0
+while cont < 5:
+    x += 1
+    print("Cont: " + str(cont))
+    cont += 1
+
+print("Valor: " + str(x)) 
